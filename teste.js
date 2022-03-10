487416c13530eda936765b65acd40d193df9f16d
Willian Stephano <will.stephano@gmail.com>
2022-03-10 18:39:44 -0300

    Alteração de icone em botão de modo escuro

--- teste.js
+++ teste.js
@@ -1,5 +1,11 @@
 function escuro() {
    var html = document.getElementById("html")
-
+   
    html.classList.toggle("claro");
+   
+   if (html.classList.contains('claro') == true) {
+      document.getElementById('bt-dark-mode').src = `Conteudo/moon.svg`
+   }else{
+      document.getElementById('bt-dark-mode').src = `Conteudo/sun-shape-svgrepo-com.svg`
+   }
 }
