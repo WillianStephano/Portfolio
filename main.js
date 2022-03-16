27376bc11e975766769a98e35f3c4e8e7758449b
Willian Stephano <will.stephano@gmail.com>
2022-03-16 16:08:21 -0300

    Criação de toggle menu em versão mobile (cheio de gambiarra por enquanto :-/ )

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 function escuro() {
-   var html = document.getElementById("html")
-   
+   const html = document.getElementById("html");
+
    html.classList.toggle("claro");
-   
-   if (html.classList.contains('claro') == true) {
-      document.getElementById('bt-dark-mode').src = `Conteudo/moon.svg`
-   }else{
-      document.getElementById('bt-dark-mode').src = `Conteudo/sun-shape-svgrepo-com.svg`
+   if (html.classList.contains("claro") == true) {
+      document.getElementById("bt-dark-mode").src = `Conteudo/moon.svg`;
+   } else {
+      document.getElementById("bt-dark-mode").src = `Conteudo/sun-shape-svgrepo-com.svg`;
    }
+}
+
+
+function toggleMenu() {
+   var btnmobile = document.getElementById("btn-mobile");
+   const menu = document.getElementById("teste");
+   menu.classList.toggle("active");
 }
