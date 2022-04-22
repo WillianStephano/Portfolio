f08b692c5b177a7c5944c5494d5c5c79bb97c9ae
Willian Stephano <will.stephano@gmail.com>
2022-04-22 14:46:20 -0300

    Mudanças em toggle menu de dispositivos moveis e melhorias em JS

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 /* Dark mode */
-function escuro() {
+const btTema = document.querySelector('.bt-tema')
+
+btTema.addEventListener('click',function () {
    const html = document.getElementById("html");
    
    html.classList.toggle("claro");
    if (html.classList.contains("claro") == true) {
-      document.getElementById("bt-dark-mode").src = `Conteudo/moon.svg`;
+      document.querySelector(".bt-tema").src = `Conteudo/moon.svg`;
    } else {
-      document.getElementById("bt-dark-mode").src = `Conteudo/sun-shape-svgrepo-com.svg`;
+      document.querySelector(".bt-tema").src = `Conteudo/sun-shape-svgrepo-com.svg`;
    }
-}
+})
+
+
 
 /* Menu mobile */
-function toggleMenu() {
-   var btnmobile = document.getElementById("btn-mobile");
+const btMobile = document.querySelector(".bt-mobile");
+
+btMobile.addEventListener('click',function () {
    const menu = document.getElementById("teste");
    menu.classList.toggle("active");
-}
+} )
+
+
 
 /* Projetos */
-
 const container = document.querySelector('.conteudo-projetos .container')
 const btProjetos = document.querySelector('.bt_secao_projetos')
 
 btProjetos.addEventListener('click',function () {
    container.classList.toggle('container-ativo')
-
-
 })
